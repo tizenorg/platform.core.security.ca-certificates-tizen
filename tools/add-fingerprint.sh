dc6bf8acc758a3257191ce3592d30f36cc6df363
kyungwook tak <k.tak@samsung.com>
2015-04-30 01:11:44 +0900

    Generate fingerprint list f rom certificates
    Change-Id: I8bd9d8709fdf588ba8b5375895b17a9330ebc100
    Signed-off-by: kyungwook tak <k.tak@samsung.com>

---
+++ tools/add-fingerprint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+CERT_ROOT=$1
+XML_PATH=$2
+
+if [ -z "$2" ]
+then
+	echo "usage: `basename $0` certificate_root_path fingerprint_xml_path"
+	exit 2
+fi
+
+for CATEGORY in developer public partner platform
+do
+	if [ -d "$CERT_ROOT/$CATEGORY" ]
+	then
+		echo
+		echo " <<$CATEGORY>>"
+		for CERT_PATH in `ls $CERT_ROOT/$CATEGORY/*.pem`
+		do
+			FINGERPRINT=`/usr/bin/openssl x509 -noout -fingerprint -in $CERT_PATH | cut -d '=' -f 2`
+			echo "  ${CERT_PATH##*/}:"
+			echo "   $FINGERPRINT"
+			##################################################################
+			# Find "<CertificateDomain name="tizen-xxxxxxx">"                #
+            # then add the fingerprint into the next line                    #
+			##################################################################
+			#        <FingerprintSHA1>[...fingerprints...]</FingerprintSHA1> #
+			##################################################################
+			/bin/sed -i "s#<CertificateDomain name=\"tizen-$CATEGORY\">.*#&\n        <FingerprintSHA1>$FINGERPRINT</FingerprintSHA1><!-- ${CERT_PATH##*/} -->#" $XML_PATH
+		done
+	fi
+done
+echo
